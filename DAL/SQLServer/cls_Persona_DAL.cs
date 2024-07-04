f1ceaee36ecacd798ed64a608503676221d9a777
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-07-03 20:15:34 -0600

    PersonaDAL

--- DAL/SQLServer/cls_Persona_DAL.cs
+++ DAL/SQLServer/cls_Persona_DAL.cs
@@ -1,26 +1,56 @@
-﻿using System;
+﻿using DAL.Interfaces;
+using Dapper;
+using Entidad.SQLServer;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace DAL.SQLServer
 {
-    public class cls_Persona_DAL
+    public class cls_Persona_DAL    : IPersona_DAL
     {
-        #region VARIABLES PUBLICAS
-        public int iCedula {get;set;}
-        public string sNombre { get; set; }
-        public string sApellido { get; set; }
-        public bool bGenero { get; set; }
-        public DateTime dtmFechaNacimiento { get; set; }
+        #region VARIABLE PRIVADA
+        private readonly IConfiguration _iConfiguracion;
+        #endregion
 
-        public cls_Persona_DAL()
+        #region CONSTRUCTOR
+        public cls_Persona_DAL(IConfiguration IConfig)
         {
-            iCedula = 0;
-            sNombre = string.Empty;
-            sApellido = string.Empty;
-            bGenero = true;
-            dtmFechaNacimiento = DateTime.MinValue;
+            _iConfiguracion = IConfig;
         }
+
+       
+        #endregion
+
+        #region METODOS
+        public List<cls_Persona> ConsultarPersona(cls_Persona Obj_Entidad)
+        {
+            DynamicParameters parametros = new DynamicParameters();
+            /*CREAMOS LOS PARAMETROS*/
+            parametros.Add("@FILTRO", Obj_Entidad.iCedula, DbType.Int64, ParameterDirection.Input);
+
+            using (var CNXSQL = new SqlConnection(_iConfiguracion.GetConnectionString("ConexionSQLServer")))
+            {
+                return (List<cls_Persona>)CNXSQL.Query<cls_Persona>("dbo.Sp_Consultar_Perfil", parametros, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public bool AgregarPersona(cls_Persona Obj_Entidad)
+        {
+            throw new NotImplementedException();
+        }
+        public bool ModificarPersona(cls_Persona Obj_Entidad)
+        {
+            throw new NotImplementedException();
+        }
+        public bool EliminarPersona(cls_Persona Obj_Entidad)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
     }
 }
--- Entidad/SQLServer/cls_Persona.cs
+++ Entidad/SQLServer/cls_Persona.cs
@@ -7,7 +7,20 @@ namespace Entidad.SQLServer
     public class cls_Persona
     {
         #region VARIABLES PUBLICAS
+        public int iCedula { get; set; }
+        public string sNombre { get; set; }
+        public string sApellido { get; set; }
+        public bool bGenero { get; set; }
+        public DateTime dtmFechaNacimiento { get; set; }
 
+        public cls_Persona()
+        {
+            iCedula = 0;
+            sNombre = string.Empty;
+            sApellido = string.Empty;
+            bGenero = true;
+            dtmFechaNacimiento = DateTime.MinValue;
+        }
         #endregion
     }
 }
