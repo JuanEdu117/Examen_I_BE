40a8dc5551e378294970d0009074f4512c8b823f
JuanEdu117 <juaned.jimenez99@gmail.com>
2024-07-03 19:42:27 -0600

    variables persona

--- DAL/SQLServer/cls_Persona_DAL.cs
+++ DAL/SQLServer/cls_Persona_DAL.cs
@@ -7,7 +7,20 @@ namespace DAL.SQLServer
     public class cls_Persona_DAL
     {
         #region VARIABLES PUBLICAS
+        public int iCedula {get;set;}
+        public string sNombre { get; set; }
+        public string sApellido { get; set; }
+        public bool bGenero { get; set; }
+        public DateTime dtmFechaNacimiento { get; set; }
 
+        public cls_Persona_DAL()
+        {
+            iCedula = 0;
+            sNombre = string.Empty;
+            sApellido = string.Empty;
+            bGenero = true;
+            dtmFechaNacimiento = DateTime.MinValue;
+        }
         #endregion
     }
 }
